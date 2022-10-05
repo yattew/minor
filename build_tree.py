f0b931aac3dca6a5e90b910da20d5976cf5be5be
yattew <yathansh@gmail.com>
2022-10-05 20:20:58 +0530

    tree retreival done

---
+++ build_tree.py
@@ -0,0 +1,62 @@
+import pickle
+
+class Tree:
+    def __init__(self, name=None):
+        self.name = name
+        self.children = {}
+        self.parents = {}
+        self.doc = ""
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def do_dfs(children: dict, tree: Tree, parent: Tree, nodes) -> Tree:
+
+    if children == None:
+        return None
+    else:
+        for i in children:
+            if i not in nodes:
+                tree.children[i] = Tree(i)
+                nodes[i] = tree.children[i]
+                tree.children[i].doc = children[i][0]
+                
+                do_dfs(children[i][1], tree.children[i], tree.children[i], nodes)
+            child = nodes[i]
+            child.parents[parent.name] = parent
+
+
+def build_tree(file):
+    tree = None
+    nodes = {}
+    import json
+    with open(file, "r") as f:
+        jsn_asDict = json.load(f)
+        tree = Tree("class")
+        tree.doc = jsn_asDict["class"][0]
+        nodes["class"] = tree
+        do_dfs(jsn_asDict["class"][1], tree, tree, nodes)
+        return tree, nodes
+
+
+def save_tree(tree, nodes, file):
+    with open(file, "wb") as f:
+        pickle.dump(tree, f, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(nodes, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_tree(file):
+    with open(file, "rb") as f:
+        a = pickle.load(f)
+        b = pickle.load(f)
+        return (a,b)
+
+tree, nodes = build_tree("tree.json")
+save_tree(tree,nodes,"tree.dat")
+# tree,nodes = load_tree("tree.dat")
+# from pprint import pprint
+# pprint(tree)
+# pprint(nodes)
--- main.py
+++ main.py
@@ -1,44 +1,51 @@
+import pickle
+
+
 class Tree:
     def __init__(self, name=None):
         self.name = name
         self.children = {}
         self.parents = {}
         self.doc = ""
+
     def __str__(self):
         return self.name
+
     def __repr__(self):
         return self.__str__()
 
 
-nodes={}
-def do_dfs(children:dict, tree:Tree,parent :Tree) -> Tree:
-    
-    if children==None:
-        return None
-    else:
-        for i in children:
-            if i in nodes:
-                child=nodes[i]
-                child.parents[parent.name]=parent
-            else:
-                tree.children[i]=Tree(i)
-                nodes[i]=tree.children[i]
-                tree.children[i].doc=children[i][0]
-                do_dfs(children[i][1],tree.children[i],tree.children[i])
-        pass
-    
-    
-    
+def load_tree(file):
+    with open(file, "rb") as f:
+        a = pickle.load(f)
+        b = pickle.load(f)
+        return (a, b)
 
-def build_tree(file):
-    tree = None
-    import json
-    with open(file,"r") as f:
-        jsn_asDict = json.load(f)
-        tree = Tree("class")
-        tree.doc=jsn_asDict["class"][0]
-        nodes["class"]=tree
-        do_dfs(jsn_asDict["class"][1],tree,tree)
-        print(jsn_asDict)
-build_tree("tree.json")
-print(nodes)
+
+def query_obj(nodes, name: str):
+    return nodes[name]
+
+
+def search_term(tree, nodes, term):
+    tree_pt = query_obj(nodes, term)
+    #read from actual document
+    doc = tree_pt.doc
+    parents = tree_pt.parents
+    children = tree_pt.children
+    return {
+        "doc":doc,
+        "parents":parents,
+        "children":children,
+        "tree_pt":tree_pt
+    }
+
+
+def main():
+    tree, nodes = load_tree("tree.dat")
+    term = "functions"
+    print(search_term(tree, nodes, term))
+
+
+
+if __name__ == "__main__":
+    main()
