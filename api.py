8e7c1256dc1e618ea0cd6c6b97ca71c8982e72b3
yattew <yathansh@gmail.com>
2022-10-05 22:01:44 +0530

    basic api working

--- api.py
+++ api.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from tree import *
 from utils import *
 
+
 app = Flask(__name__)
+config = get_config()
+tree, nodes = load_tree(config["tree_serialized"])
+
+
+@app.route("/query/<query>")
+def query(query):
+    result = search_term(tree, nodes, query)
+    return result["tree_pt"].name
+
 
 if __name__ == "__main__":
-    config = get_config()
     debug = False
     if config["env"] == "dev":
         debug = True
-    app.run(debug=debug)
+    app.run(debug=debug, host=config["host"], port=config["port"])
--- tree.py
+++ tree.py
@@ -57,12 +57,35 @@ def load_tree(file):
 
 
 
+def query_obj(nodes, name: str):
+    return nodes[name]
+
+
+def search_term(tree, nodes, term):
+    tree_pt = query_obj(nodes, term)
+    # read from actual document
+    doc = tree_pt.doc
+    parents = tree_pt.parents
+    children = tree_pt.children
+    return {
+        "doc": doc,
+        "parents": parents,
+        "children": children,
+        "tree_pt": tree_pt
+    }
+
+
+
 if __name__ == "__main__":
     from utils import *
     config = get_config()
     
     tree, nodes = build_tree(config["tree"])
     save_tree(tree,nodes,config["tree_serialized"])
+    # config = get_config()
+    # tree, nodes = load_tree(config["tree_serialized"])
+    # term = "functions"
+    # print(search_term(tree, nodes, term))
     # tree,nodes = load_tree("tree.dat")
     # from pprint import pprint
     # pprint(tree)
