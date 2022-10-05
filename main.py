e8d588f39f370669105a9483c7ab897935d9f7f6
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-10-05 17:00:28 +0530

    created tree

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ class Tree:
         self.children = {}
         self.parents = {}
         self.doc = ""
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return self.__str__()
 
 
-
-def do_dfs(root:dict, tree:Tree) -> Tree:
-    children = root[1]
-    if not children:
+nodes={}
+def do_dfs(children:dict, tree:Tree,parent :Tree) -> Tree:
+    
+    if children==None:
         return None
+    else:
+        for i in children:
+            if i in nodes:
+                child=nodes[i]
+                child.parents[parent.name]=parent
+            else:
+                tree.children[i]=Tree(i)
+                nodes[i]=tree.children[i]
+                tree.children[i].doc=children[i][0]
+                do_dfs(children[i][1],tree.children[i],tree.children[i])
+        pass
     
     
     
@@ -19,9 +34,11 @@ def build_tree(file):
     tree = None
     import json
     with open(file,"r") as f:
-        jsn = json.load(f)
-        root = jsn["class"]
+        jsn_asDict = json.load(f)
         tree = Tree("class")
-        do_dfs(root,tree)
-        print(jsn)
+        tree.doc=jsn_asDict["class"][0]
+        nodes["class"]=tree
+        do_dfs(jsn_asDict["class"][1],tree,tree)
+        print(jsn_asDict)
 build_tree("tree.json")
+print(nodes)
