b7f35c644a568b4b605802b1b2144eece56c5a31
yattew <yathansh@gmail.com>
2022-10-18 23:50:35 +0530

    forgor something

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -3,11 +3,11 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className={styles.navContent}>
-                <div>
+                <div className={styles.leftContent}>
                     <div className="navbar-brand">Python-Help</div>
                 </div>
                 <input className="form-control" type="search" placeholder="Search" />
-                <div>
+                <div className={styles.rightContent}>
                     <button className="btn btn-primary">Help</button>
                 </div>
             </div>
