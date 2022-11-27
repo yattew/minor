44ef20526a8d6aa0ed182bdccc7f817b536a7edc
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-11-27 16:46:42 +0530

    Update cosines.py

--- scripts/cosines.py
+++ scripts/cosines.py
@@ -1,5 +1,14 @@
+from build_vocab import *
 from stopstem import *
 def cosine(string1,string2):
-    vocab=[]
+    vocab = []
+    vocabDict = {}
     with open("vocab.txt" ,'r') as f:
         vocab = f.read().split(',')
+        for i in range(len(vocab)):
+            vocabDict[vocab[i]] = i
+    str1Arr = tokenizeAndStem(string1)
+    str2Arr = tokenizeAndStem(string2)
+    embed1 = [0]*len(vocab)
+    embed2 = [0]*len(vocab)
+    for i in range(str1Arr):
