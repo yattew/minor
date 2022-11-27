6aa7f52df329072812a25e68b6079dcf52ef88b6
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-11-27 16:57:02 +0530

    Cosine similarity done

--- scripts/cosines.py
+++ scripts/cosines.py
@@ -1,5 +1,18 @@
 from build_vocab import *
 from stopstem import *
+def cacCosine(arr1,arr2):
+    dot = 0
+    mag1 = 0
+    mag2 = 0
+    for i in range(len(arr1)):
+        dot += arr1[i]*arr2[i]
+        mag1 += arr1[i]**2
+        mag2 += arr2[i]**2
+    mag1 = mag1**(1/2)
+    mag2 = mag2**(1/2)
+    return dot/(mag1*mag2)
+
+
 def cosine(string1,string2):
     vocab = []
     vocabDict = {}
@@ -9,6 +22,14 @@ def cosine(string1,string2):
             vocabDict[vocab[i]] = i
     str1Arr = tokenizeAndStem(string1)
     str2Arr = tokenizeAndStem(string2)
-    embed1 = [0]*len(vocab)
-    embed2 = [0]*len(vocab)
-    for i in range(str1Arr):
+    oneHot1 = [0]*len(vocab)
+    oneHot2 = [0]*len(vocab)
+    for i in str1Arr:
+        if i in vocabDict:
+            oneHot1[vocabDict[i]]+=1
+    for i in str2Arr:
+        if i in vocabDict:
+            oneHot2[vocabDict[i]]+=1
+    result = calcCosine(oneHot1,oneHot2)
+    return result
+
