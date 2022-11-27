0c6881a0899c19f315638ff617f49498d347b4eb
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-11-27 21:00:25 +0530

    added new doc

--- scripts/create_more_dataset.py
+++ scripts/create_more_dataset.py
@@ -8,7 +8,7 @@ path = "C://Users//ganes//Desktop//mini//jsons"
 dir_list = os.listdir(path)
 
 print(dir_list)
-
+vocab = ' '
 for i in dir_list:
     if i[-4::] == 'json':
         with open(BASE_DIR/("jsons/"+i)) as data_source:
@@ -18,9 +18,11 @@ for i in dir_list:
             for j in data1:
                 vocabulary.extend(data1[j]["reading"].keys())
                 vocabulary.extend(data1[j]["videos"].keys())
-            vocab = ' '.join(vocabulary)
+            vocab += ' '.join(vocabulary) + ' '
             # print(vocab)
             # print(BASE_DIR/("reference/dataset_"+i[:-4]+'txt'))
-            with open(BASE_DIR/("reference/dataset_"+i[:-4]+'txt'),'w') as target:
-                target.write(vocab)
+            # with open(BASE_DIR/("reference/dataset_"+i[:-4]+'txt'),'w') as target:
+            #     target.write(vocab)
+with open(BASE_DIR/("reference/dataset__dataset.txt"),'w') as target:
+    target.write(vocab)
 #print(vocabulary)
